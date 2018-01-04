ef8da45b45744f526c46c04b58d08697d8d0e010
Declan Valters <declan.valters@ed.ac.uk>
2018-01-04 16:38:36 +0000

    Added some more netcdf examples

--- landsea_mask.py
+++ landsea_mask.py
@@ -13,11 +13,39 @@ values are sea, and everything else is land.
 from netCDF4 import Dataset
 
 dataset = Dataset('/home/dvalters/Datasets/WFDEI_global_dyn.month.2d.gpp_gb.2000.1.nc',
-                  'w', format='NETCDF4_CLASSIC')
-                  
+                  format='NETCDF4_CLASSIC')
+                 
+# print the netcdf file format type
 print(dataset.file_format)
 
-#for key, value in dataset.variables.items():
-#    print(key)
+for key, value in dataset.variables.items():
+    print(key)
 
-#level = dataset.createDimension('level)
+# print the variables in the netcdf dataset
+# i.e. the 'fields' as some meteorologists call them...
+print(dataset.variables)
+
+# print all the dimensions in the dataset
+for dimobj in dataset.dimensions.values():
+    print(dimobj)
+
+# prints the lat an longitude dimensions, and their size
+print(dataset.dimensions['lat'])
+print(dataset.dimensions['lon'])
+
+#print the variable, GPP (gross primary production)
+print(dataset.variables['gpp'])
+
+# print any data storage conventions used (e.g. CF-1.5)
+print(dataset.Conventions)
+
+# Get a list of any attributes in the dataset (global attributes)
+for attr in dataset.ncattrs():
+    print(attr, '=', getattr(dataset, attr))
+
+# Assign the variable GPP to an object name
+GPP = dataset.variables['gpp']
+
+# Get a list of variable attributes for GPP
+for attr in GPP.ncattrs():
+    print(attr, '=', getattr(GPP, attr))
