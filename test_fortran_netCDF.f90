3949df3291a7294b1c0eb46da65ffd6f43c68842
Declan Valters <declan.valters@ed.ac.uk>
2018-01-17 09:36:04 +0000

    Subroutine for error handling

--- test_fortran_netCDF.f90
+++ test_fortran_netCDF.f90
@@ -10,12 +10,19 @@ integer :: ncId, rhVarId, status
 real    :: rhValue
 
 status = nf90_open("JULES_WFDEI_global_dyn_ALL.nc", nf90_NoWrite, ncid)
-if (status /= nf90_NoErr) call handle_err(status)
+if (status /= nf90_NoErr) call handle_err('nf90_open', status)
 
 status = nf90_inq_varid(ncid, "latitude", rhVarId)
-if (status /= nf90_NoErr) call handle_err(status)
+if (status /= nf90_NoErr) call handle_err('nf90_inq_varid', status)
 
 status = nf90_get_var(ncid, rhVarId, rhValue, start = (/4, 3, 3 /))
-if (status /= nf90_NoErr) call handle_err(status)
+if (status /= nf90_NoErr) call handle_err('nf90_get_var', status)
+
+contains
+  subroutine handle_err(funcname, status)
+    character(len=*)    ::  funcname
+    integer          ::  status
+    print *, 'ERR: Calling ', funcname, ' returned error code: ', status
+  end subroutine handle_err
 
 end program test_netcdf
