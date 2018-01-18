f4346a51670ad16ebc0b9da42d7ade9e48cbe0da
Declan Valters <declan.valters@ed.ac.uk>
2018-01-18 16:17:16 +0000

    Changed test variable name (sratiform precip)

--- test_fortran_netCDF_transpose.f90
+++ test_fortran_netCDF_transpose.f90
@@ -13,15 +13,15 @@ real, dimension(numLons, numLats)  :: rhValues
 
 status = nf90_open("/disk/scratch/local/dvalters/netcdf_test/ECMWF_ERA-40_subset.nc", &
        nf90_NoWrite, ncid)
-if (status /= nf90_NoErr) call handle_err(status)
+if (status /= nf90_NoErr) call handle_err('nf90_open err ', status)
 
-status = nf90_inq_varid(ncid, "rh", rhVarId)
-if(status /= nf90_NoErr) call handle_err(status)
+status = nf90_inq_varid(ncid, "lsp", rhVarId)
+if(status /= nf90_NoErr) call handle_err('nf90_inq_var err ', status)
 
 ! Read transposed values: map vector would be (/ 1, numLats /) for
 !   no transposition
 status = nf90_get_var(ncid, rhVarId,rhValues) !, map = (/ numLons, 1 /))
-if(status /= nf90_NoErr) call handle_err(status)
+if(status /= nf90_NoErr) call handle_err('nf90_get_var err: ', status)
 
 end program transpose_nf90
 
